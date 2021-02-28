57b670443ddc364a68a5b808005b21f5363589d6
Alvin Zhang <mosoft521@gmail.com>
2021-02-28 17:28:16 +0800

    22-Enumerations

---
+++ enums/src/main/java/enums/SpicinessEnum.java
@@ -0,0 +1,8 @@
+package enums;// enums/enums.SpicinessEnum.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+
+public enum SpicinessEnum {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+}
---
+++ enums/src/main/java/enums/menu/TypeOfFood.java
@@ -0,0 +1,20 @@
+// enums/menu/TypeOfFood.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// {java enums.menu.TypeOfFood}
+package enums.menu;
+
+import static enums.menu.Food.Appetizer;
+import static enums.menu.Food.Coffee;
+import static enums.menu.Food.Dessert;
+import static enums.menu.Food.MainCourse;
+
+public class TypeOfFood {
+    public static void main(String[] args) {
+        Food food = Appetizer.SALAD;
+        food = MainCourse.LASAGNE;
+        food = Dessert.GELATO;
+        food = Coffee.CAPPUCCINO;
+    }
+}
