fb0a44258005a0eeaa44fb567b86b2348593a016
Alvin Zhang <mosoft521@gmail.com>
2021-02-28 17:51:15 +0800

    Appendix-Collection-Topics

---
+++ collectiontopics/src/main/java/QueueBehavior.java
@@ -0,0 +1,64 @@
+// collectiontopics/QueueBehavior.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// Compares basic behavior
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.stream.Stream;
+
+public class QueueBehavior {
+    static Stream<String> strings() {
+        return Arrays.stream(
+                ("one two three four five six seven " +
+                        "eight nine ten").split(" "));
+    }
+
+    static void test(int id, Queue<String> queue) {
+        System.out.print(id + ": ");
+        strings().map(queue::offer).count();
+        while (queue.peek() != null)
+            System.out.print(queue.remove() + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int count = 10;
+        test(1, new LinkedList<>());
+        test(2, new PriorityQueue<>());
+        test(3, new ArrayBlockingQueue<>(count));
+        test(4, new ConcurrentLinkedQueue<>());
+        test(5, new LinkedBlockingQueue<>());
+        test(6, new PriorityBlockingQueue<>());
+        test(7, new ArrayDeque<>());
+        test(8, new ConcurrentLinkedDeque<>());
+        test(9, new LinkedBlockingDeque<>());
+        test(10, new LinkedTransferQueue<>());
+        test(11, new SynchronousQueue<>());
+    }
+}
+/* Output:
+1: one two three four five six seven eight nine ten
+2: eight five four nine one seven six ten three two
+3: one two three four five six seven eight nine ten
+4: one two three four five six seven eight nine ten
+5: one two three four five six seven eight nine ten
+6: eight five four nine one seven six ten three two
+7: one two three four five six seven eight nine ten
+8: one two three four five six seven eight nine ten
+9: one two three four five six seven eight nine ten
+10: one two three four five six seven eight nine ten
+11:
+*/
