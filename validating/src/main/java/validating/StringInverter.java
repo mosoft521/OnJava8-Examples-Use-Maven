7e9b4a7a107b4bba5cb3b5b26dfc665cf69df60d
Alvin Zhang <mosoft521@gmail.com>
2021-03-02 18:14:37 +0800

    添加中文注释

--- validating/src/main/java/validating/StringInverter.java
+++ validating/src/main/java/validating/StringInverter.java
@@ -3,7 +3,8 @@
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 package validating;
-
+//功能是反转 String 中字符的大小写。
+// 约束：String 必须小于或等于30个字符，并且必须只包含字母，空格，逗号和句号(英文)。
 interface StringInverter {
     String invert(String str);
 }
