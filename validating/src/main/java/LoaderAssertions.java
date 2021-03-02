d783b82fc270204da30e4b58cc94483e138851a4
Alvin Zhang <mosoft521@gmail.com>
2021-03-02 17:47:26 +0800

    程序开启Assertion的两种方式

--- validating/src/main/java/LoaderAssertions.java
+++ validating/src/main/java/LoaderAssertions.java
@@ -6,7 +6,19 @@
 // {ThrowsException}
 
 public class LoaderAssertions {
+    //方式一：决定在程序运行时启用断言assertion可以使用下面的 static 块来实现这一点，该语句位于系统的主类中：
+    static {
+        boolean assertionsEnabled = false;
+        // Note intentional side effect of assignment:
+        assert assertionsEnabled = true;
+        if (!assertionsEnabled)
+            throw new RuntimeException("Assertions disabled");
+    }
+
     public static void main(String[] args) {
+        //方式二：通过编程的方式通过链接到类加载器对象（ClassLoader）来控制断言。
+        // 类加载器中有几种方法允许动态启用和禁用断言，其中 setDefaultAssertionStatus () ,
+        // 它为之后加载的所有类设置断言状态。
         ClassLoader.getSystemClassLoader()
                 .setDefaultAssertionStatus(true);
         new Loaded().go();
