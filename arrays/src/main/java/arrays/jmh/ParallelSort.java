4b19b549d16e942a038c5a0f9adc0a6be74d8088
Alvin Zhang <mosoft521@gmail.com>
2021-03-04 17:53:23 +0800

    arrays.jmh

--- arrays/src/main/java/arrays/jmh/ParallelSort.java
+++ arrays/src/main/java/arrays/jmh/ParallelSort.java
@@ -6,13 +6,26 @@ package arrays.jmh;
 
 import onjava.Rand;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+// Increase these three for more accuracy:
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@Fork(1)
 public class ParallelSort {
     private long[] la;
 
