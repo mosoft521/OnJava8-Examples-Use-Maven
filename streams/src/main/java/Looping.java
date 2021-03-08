e498b314a11fe1d49542de908a896a52fb8bb7ce
Alvin Zhang <mosoft521@gmail.com>
2021-03-08 09:33:53 +0800

    prettify 10

--- streams/src/main/java/Looping.java
+++ streams/src/main/java/Looping.java
@@ -3,6 +3,7 @@
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 
+import static java.util.stream.IntStream.range;
 import static onjava.Repeat.repeat;
 
 public class Looping {
@@ -11,8 +12,10 @@ public class Looping {
     }
 
     public static void main(String[] args) {
-        repeat(3, () -> System.out.println("Looping!"));
+        repeat(3, () -> System.out.println("Looping!"));//[1]
         repeat(2, Looping::hi);
+
+        range(0, 3).forEach(i -> System.out.println("Looping " + i));//[2]把[1]展开
     }
 }
 /* Output:
@@ -21,4 +24,7 @@ Looping!
 Looping!
 Hi!
 Hi!
+Looping!
+Looping!
+Looping!
 */
